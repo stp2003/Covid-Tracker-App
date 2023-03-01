9a5d9a38f15a850746bea6f9835250274001e266
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-03-01 18:51:38 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:covid_tracker_app/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Covid Tracker App Flutter.',
+      theme: ThemeData.dark().copyWith(),
+      home: const SplashScreen(),
+    );
+  }
+}
